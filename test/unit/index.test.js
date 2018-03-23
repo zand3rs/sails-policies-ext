b2a69a63dd7ece5ef1df20fabaabebc79d82ee8d
zander <aamagtipon@voyagerinnovation.com>
2018-03-23 17:14:36 +0800

    more comprehensive unit tests

--- test/unit/index.test.js
+++ test/unit/index.test.js
@@ -15,6 +15,39 @@ describe(TEST_NAME, function() {
     });
   });
 
+  describe(".Policies()", function() {
+    describe("constructor", function() {
+      it("accepts folder parameter", function() {
+        const dirPath = "/tmp/policies";
+        const policies = new Self.Policies(dirPath);
+
+        expect(policies).to.have.property("folder", dirPath);
+      });
+    });
+
+    describe("instance members", function() {
+      const policies = new Self.Policies();
+
+      it("#folder should exist", function() {
+        expect(policies).to.have.property("folder").that.is.a("string");
+      });
+
+      it("#middlewares should exist", function() {
+        expect(policies).to.have.property("middlewares").that.is.a("array");
+      });
+    });
+
+    describe("dynaminc instance members", function() {
+      it("should have methods for the loaded policies", function() {
+        const folder = path.join(TEST_FIXTURES_PATH, "policies");
+        const policies = new Self.Policies(folder);
+
+        expect(policies).to.respondTo("allow");
+        expect(policies).to.respondTo("isLoggedIn");
+      });
+    });
+  });
+
   describe(".load()", function() {
     it("should return instance of Policies", function() {
       expect(Self.load()).to.be.instanceof(Self.Policies);
--- test/unit/lib/policy.test.js
+++ test/unit/lib/policy.test.js
@@ -5,8 +5,40 @@ var Self = require(path.join(process.cwd(), "lib", "policy"));
 
 describe(TEST_NAME, function() {
 
+  describe("constructor", function() {
+    it("throws an error if file path is not provided", function() {
+      expect(() => new Self()).to.throw(TypeError, /Path must be a strin/);
+    });
+
+    it("throws an error if file path is empty string", function() {
+      expect(() => new Self("")).to.throw(Error, /missing path/);
+    });
+
+    it("throws an error if file cannot be loaded", function() {
+      const fpath = path.join(TEST_FIXTURES_PATH, "policies", "dummy.js");
+      expect(() => new Self(fpath)).to.throw(Error, /Cannot find module/);
+    });
+
+    it("loads the given file path", function() {
+      const fpath = path.join(TEST_FIXTURES_PATH, "policies", "allow.js");
+      expect(new Self(fpath)).to.be.an("object");
+    });
+  });
+
   describe("instance members", function() {
+    const fpath = path.join(TEST_FIXTURES_PATH, "policies", "allow.js");
+    const policy = new Self(fpath);
+
+    it("#name should exist", function() {
+      expect(policy).to.have.property("name", "allow");
+    });
+
+    it("#module should exist", function() {
+      expect(policy).to.have.property("module").that.is.a("function").with.length(1);
+    });
+
     it("#apply() should exist", function() {
+      expect(policy).to.respondTo("apply");
     });
   });
 
